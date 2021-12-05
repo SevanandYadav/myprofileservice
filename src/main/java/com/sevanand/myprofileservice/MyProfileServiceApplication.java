782c48a53b600f62f9488e99cac4edf0a2485520
Sevanand Yadav <sevanandyadav@gmail.com>
2021-12-05 19:09:06 +0530

    Heroku build fix
    Heroku build fix

--- src/main/java/com/sevanand/myprofileservice/MyProfileServiceApplication.java
+++ src/main/java/com/sevanand/myprofileservice/MyProfileServiceApplication.java
@@ -3,6 +3,7 @@ package com.sevanand.myprofileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
